80abcc5d8c34fc3e958016abeaeb04d27d0f37c1
Cameron Lane <crlane@admanteus.com>
2024-03-13 09:44:24 -0400

    added nvim

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,423 @@
+--  KeyMappings ------------------
+-- remap leader to spacebar
+vim.keymap.set("n", "<Space>", "<Nop>", { silent = true })
+vim.g.mapleader = " "
+
+-- quick-open
+vim.keymap.set('', '<C-p>', '<cmd>Files<cr>')
+-- search buffers
+vim.keymap.set('n', '<leader>;', '<cmd>Buffers<cr>')
+
+-- jj
+local options = { nowait = true, noremap = true}
+vim.keymap.set('i', 'jj', '<Esc>', options)
+vim.keymap.set('v', 'jj', '<Esc>', options)
+-- vim.keymap.set('s', 'jj', '<Esc>')
+-- vim.keymap.set('x', 'jj', '<Esc>')
+-- vim.keymap.set('c', 'jj', '<Esc>')
+-- vim.keymap.set('o', 'jj', '<Esc>')
+-- vim.keymap.set('l', 'jj', '<Esc>')
+-- vim.keymap.set('t', 'jj', '<Esc>')
+
+-- <leader>p will paste clipboard into buffer
+-- <leader>c will copy entire buffer into clipboard
+-- TODO: set pastemode first
+vim.keymap.set('n', '<leader>p', '<cmd>read !wl-paste<cr>')
+vim.keymap.set('n', '<leader>c', '<cmd>w !wl-copy<cr><cr>')
+
+-- <leader><leader> to stop searching
+vim.keymap.set('v', '<leader><leader>', '<cmd>nohlsearch<cr>')
+vim.keymap.set('n', '<leader><leader>', '<cmd>nohlsearch<cr>')
+
+-- better window navigation
+local options = { silent = true, noremap = true }
+vim.keymap.set('n', '<C-h>', '<cmd>wincmd h<cr>', options)
+vim.keymap.set('n', '<C-j>', '<cmd>wincmd j<cr>', options)
+vim.keymap.set('n', '<C-k>', '<cmd>wincmd k<cr>', options)
+vim.keymap.set('n', '<C-l>', '<cmd>wincmd l<cr>', options)
+-------------------------------------------------------------
+
+------- Options
+-- relative line numbers
+vim.opt.relativenumber = true
+-- and show the absolute line number for the current line
+vim.opt.number = true
+-- keep current content top + left when splitting
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+-- infinite undo!
+-- NOTE: ends up in ~/.local/state/nvim/undo/
+vim.opt.undofile = true
+
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
+
+-- case-insensitive search/replace
+vim.opt.ignorecase = true
+-- unless uppercase in search term
+vim.opt.smartcase = true
+
+-- never ever make my terminal beep
+vim.opt.vb = true
+
+-- show a column at 80 characters as a guide for long lines
+vim.opt.colorcolumn = '80'
+--- except in Rust where the rule is 100 characters
+vim.api.nvim_create_autocmd('Filetype', { pattern = 'rust', command = 'set colorcolumn=100' })
+
+-- never ever folding
+vim.opt.foldenable = false
+vim.opt.foldmethod = 'manual'
+vim.opt.foldlevelstart = 99
+
+-- keep more context on screen while scrolling
+vim.opt.scrolloff = 2
+-- never show me line breaks if they're not there
+vim.opt.wrap = false
+-- always draw sign column. prevents buffer moving when adding/deleting sign
+vim.opt.signcolumn = 'yes'
+
+--" Decent wildmenu
+-- in completion, when there is more than one match,
+-- list all matches, and only complete to longest common match
+vim.opt.wildmode = 'list:longest'
+-- when opening a file with a command (like :e),
+-- don't suggest files like there:
+vim.opt.wildignore = '.hg,.svn,*~,*.png,*.jpg,*.gif,*.min.js,*.swp,*.o,vendor,dist,_site'
+
+-------------------------------------------------------------------------------
+--
+-- autocommands
+--
+-------------------------------------------------------------------------------
+-- leave paste mode when leaving insert mode (if it was on)
+vim.api.nvim_create_autocmd('InsertLeave', { pattern = '*', command = 'set nopaste' })
+
+
+--
+-------------------------------------------------------------------------------
+--
+-- plugin configuration
+--
+-------------------------------------------------------------------------------
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+-- then, setup!
+require("lazy").setup({
+	-- main color scheme
+	-- nice bar at the bottom
+	{
+		'itchyny/lightline.vim',
+		lazy = false, -- also load at start since it's UI
+		config = function()
+			-- no need to also show mode in cmd line when we have bar
+			vim.o.showmode = false
+			vim.g.lightline = {
+				active = {
+					left = {
+						{ 'mode', 'paste' },
+						{ 'readonly', 'filename', 'modified' }
+					},
+					right = {
+						{ 'lineinfo' },
+						{ 'percent' },
+						{ 'fileencoding', 'filetype' }
+					},
+				},
+				component_function = {
+					filename = 'LightlineFilename'
+				},
+			}
+			function LightlineFilenameInLua(opts)
+				if vim.fn.expand('%:t') == '' then
+					return '[No Name]'
+				else
+					return vim.fn.getreg('%')
+				end
+			end
+			-- https://github.com/itchyny/lightline.vim/issues/657
+			vim.api.nvim_exec(
+				[[
+				function! g:LightlineFilename()
+					return v:lua.LightlineFilenameInLua()
+				endfunction
+				]],
+				true
+			)
+		end
+	},
+	-- quick navigation
+	{
+		'ggandor/leap.nvim',
+		config = function()
+			require('leap').create_default_mappings()
+		end
+	},
+	-- better %
+	{
+		'andymass/vim-matchup',
+		config = function()
+			vim.g.matchup_matchparen_offscreen = { method = "popup" }
+		end
+	},
+	-- auto-cd to root of git project
+	-- 'airblade/vim-rooter'
+	{
+		'notjedi/nvim-rooter.lua',
+		config = function()
+			require('nvim-rooter').setup()
+		end
+	},
+	-- fzf support for ^p
+	{
+		'junegunn/fzf.vim',
+		dependencies = {
+			{ 'junegunn/fzf', dir = '~/.fzf', build = './install --all' },
+		},
+		config = function()
+			-- stop putting a giant window over my editor
+			vim.g.fzf_layout = { down = '~20%' }
+			-- when using :Files, pass the file list through
+			--
+			--   https://github.com/jonhoo/proximity-sort
+			--
+			-- to prefer files closer to the current file.
+			function list_cmd()
+				local base = vim.fn.fnamemodify(vim.fn.expand('%'), ':h:.:S')
+				if base == '.' then
+					-- if there is no current file,
+					-- proximity-sort can't do its thing
+					return 'fd --type file --follow'
+				else
+					return vim.fn.printf('fd --type file --follow | proximity-sort %s', vim.fn.shellescape(vim.fn.expand('%')))
+				end
+			end
+			vim.api.nvim_create_user_command('Files', function(arg)
+				vim.fn['fzf#vim#files'](arg.qargs, { source = list_cmd(), options = '--tiebreak=index' }, arg.bang)
+			end, { bang = true, nargs = '?', complete = "dir" })
+		end
+	},
+	{
+	   'tpope/fugitive',
+	   cmd = "Git"
+	},
+	{
+	   'airblade/vim-gitgutter',
+	},
+	{
+	   'tomtom/tcomment_vim',
+	},
+	-- LSP
+	{
+		'neovim/nvim-lspconfig',
+		config = function()
+			-- Setup language servers.
+			local lspconfig = require('lspconfig')
+
+			-- Rust
+			lspconfig.rust_analyzer.setup {
+				-- Server-specific settings. See `:help lspconfig-setup`
+				settings = {
+					["rust-analyzer"] = {
+						cargo = {
+
+						},
+						imports = {
+							group = {
+								enable = false,
+							},
+						},
+						completion = {
+							postfix = {
+								enable = false,
+							},
+						},
+					},
+				},
+			}
+
+			-- Bash LSP
+			local configs = require 'lspconfig.configs'
+			if not configs.bash_lsp and vim.fn.executable('bash-language-server') == 1 then
+				configs.bash_lsp = {
+					default_config = {
+						cmd = { 'bash-language-server', 'start' },
+						filetypes = { 'sh' },
+						root_dir = require('lspconfig').util.find_git_ancestor,
+						init_options = {
+							settings = {
+								args = {}
+							}
+						}
+					}
+				}
+			end
+			if configs.bash_lsp then
+				lspconfig.bash_lsp.setup {}
+			end
+
+			-- Global mappings.
+			-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+			vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
+			vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+			vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+			vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
+
+			-- Use LspAttach autocommand to only map the following keys
+			-- after the language server attaches to the current buffer
+			vim.api.nvim_create_autocmd('LspAttach', {
+				group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+				callback = function(ev)
+					-- Enable completion triggered by <c-x><c-o>
+					vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+					-- Buffer local mappings.
+					-- See `:help vim.lsp.*` for documentation on any of the below functions
+					local opts = { buffer = ev.buf }
+					vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+					vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+					vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+					vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+					vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+					vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
+					vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
+					vim.keymap.set('n', '<leader>wl', function()
+						print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+					end, opts)
+					--vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+					vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, opts)
+					vim.keymap.set({ 'n', 'v' }, '<leader>a', vim.lsp.buf.code_action, opts)
+					vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+					vim.keymap.set('n', '<leader>f', function()
+						vim.lsp.buf.format { async = true }
+					end, opts)
+
+					local client = vim.lsp.get_client_by_id(ev.data.client_id)
+
+					-- When https://neovim.io/doc/user/lsp.html#lsp-inlay_hint stabilizes
+					-- *and* there's some way to make it only apply to the current line.
+					-- if client.server_capabilities.inlayHintProvider then
+					--     vim.lsp.inlay_hint(ev.buf, true)
+					-- end
+
+					-- None of this semantics tokens business.
+					-- https://www.reddit.com/r/neovim/comments/143efmd/is_it_possible_to_disable_treesitter_completely/
+					client.server_capabilities.semanticTokensProvider = nil
+				end,
+			})
+		end
+	},
+    {
+		"hrsh7th/nvim-cmp",
+		-- load cmp on InsertEnter
+		event = "InsertEnter",
+		-- these dependencies will only be loaded when cmp loads
+		-- dependencies are always lazy-loaded unless specified otherwise
+		dependencies = {
+			'neovim/nvim-lspconfig',
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+		},
+		config = function()
+			local cmp = require'cmp'
+			cmp.setup({
+				snippet = {
+					-- REQUIRED by nvim-cmp. get rid of it once we can
+					expand = function(args)
+						vim.fn["vsnip#anonymous"](args.body)
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({
+					['<C-b>'] = cmp.mapping.scroll_docs(-4),
+					['<C-f>'] = cmp.mapping.scroll_docs(4),
+					['<C-Space>'] = cmp.mapping.complete(),
+					['<C-e>'] = cmp.mapping.abort(),
+					-- Accept currently selected item.
+					-- Set `select` to `false` to only confirm explicitly selected items.
+					['<CR>'] = cmp.mapping.confirm({ select = true }),
+				}),
+				sources = cmp.config.sources({
+					{ name = 'nvim_lsp' },
+				}, {
+					{ name = 'path' },
+				}),
+				experimental = {
+					ghost_text = true,
+				},
+			})
+
+			-- Enable completing paths in :
+			cmp.setup.cmdline(':', {
+				sources = cmp.config.sources({
+					{ name = 'path' }
+				})
+			})
+		end
+	},
+	-- language support
+	-- terraform
+	{
+		'hashivim/vim-terraform',
+		ft = { "terraform" },
+	},
+	-- svelte
+	{
+		'evanleck/vim-svelte',
+		ft = { "svelte" },
+	},
+	-- toml
+	'cespare/vim-toml',
+	-- jinja
+	'lepture/vim-jinja',
+	-- yaml
+	{
+		"cuducos/yaml.nvim",
+		ft = { "yaml" },
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+		},
+	},
+	-- rust
+	{
+		'rust-lang/rust.vim',
+		ft = { "rust" },
+		config = function()
+			vim.g.rustfmt_autosave = 1
+			vim.g.rustfmt_emit_files = 1
+			vim.g.rustfmt_fail_silently = 0
+			vim.g.rust_clip_command = 'wl-copy'
+		end
+	},
+	-- markdown
+	{
+		'plasticboy/vim-markdown',
+		ft = { "markdown" },
+		dependencies = {
+			'godlygeek/tabular',
+		},
+		config = function()
+			-- never ever fold!
+			vim.g.vim_markdown_folding_disabled = 1
+			-- support front-matter in .md files
+			vim.g.vim_markdown_frontmatter = 1
+			-- 'o' on a list item should insert at same level
+			vim.g.vim_markdown_new_list_item_indent = 0
+			-- don't add bullets when wrapping:
+			-- https://github.com/preservim/vim-markdown/issues/232
+			vim.g.vim_markdown_auto_insert_bullets = 0
+		end
+	},
+})
