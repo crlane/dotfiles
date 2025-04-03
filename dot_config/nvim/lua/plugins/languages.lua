ece36f9f7edcf085bf3616452f2a39b144c11857
Cameron Lane <crlane@adamanteus.com>
2025-04-03 11:40:31 +0000

    add golang language

--- dot_config/nvim/lua/plugins/languages.lua
+++ dot_config/nvim/lua/plugins/languages.lua
@@ -56,5 +56,19 @@ return {
             -- https://github.com/preservim/vim-markdown/issues/232
             vim.g.vim_markdown_auto_insert_bullets = 0
         end
-    }
+    },
+    {
+        'ray-x/go.nvim',
+        dependencies = {  -- optional packages
+            "ray-x/guihua.lua",
+            "neovim/nvim-lspconfig",
+            "nvim-treesitter/nvim-treesitter",
+          },
+          config = function()
+            require("go").setup()
+          end,
+          event = {"CmdlineEnter"},
+          ft = {"go", 'gomod'},
+          build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+    },
 }
