fd48df1ba21e01e31ad9c83654ca0c5e31858b12
Cameron Lane <crlane@adamanteus.com>
2025-04-16 06:43:13 -0400

    Changes to rooter

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -93,7 +93,13 @@ require('lazy').setup({
         {
             'notjedi/nvim-rooter.lua',
             config = function()
-                require('nvim-rooter').setup()
+                require('nvim-rooter').setup({
+                    rooter_patterns = { '.git', '.hg', '.svn' },
+                    trigger_patterns = { '*' },
+                    manual = false,
+                    fallback_to_parent = true,
+                    cd_scope = 'smart',
+                })
             end,
         },
         {
