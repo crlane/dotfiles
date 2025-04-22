f2a8db1a5e0ae1a3bf4b342b3160c51a9086fa46
Cameron Lane <crlane@adamanteus.com>
2025-04-22 07:47:08 -0400

    remove uneeded ts installs

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,47 +1,45 @@
 return {
-  {
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    opts = {
-      highlight = {
-        enable = true,
-        disable = { 'python', 'go', 'lua' },
-      },
-      ensure_installed = {
-        'go',
-        'lua',
-        'markdown',
-        'python',
-        'rust',
-        'zig',
-        -- shells
-        'bash',
-        -- config langs
-        'json',
-        'json5',
-        'jsonc',
-        'yaml',
-        'toml',
-        'ini',
-        'csv',
-        'tsv',
-        -- doc langs
-        'latex',
-        'bibtex',
-        'rst',
-        'markdown',
-        'markdown_inline',
-        -- git
-        'git_config',
-        'git_rebase',
-        'gitattributes',
-        'gitcommit',
-        'gitignore',
-      },
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = ':TSUpdate',
+        opts = {
+            highlight = {
+                enable = true,
+                disable = { 'python', 'go', 'lua' },
+            },
+            ensure_installed = {
+                'go',
+                'lua',
+                'markdown',
+                'python',
+                'rust',
+                'zig',
+                -- shells
+                'bash',
+                -- config langs
+                'json',
+                'yaml',
+                'toml',
+                'ini',
+                'csv',
+                'tsv',
+                -- doc langs
+                'latex',
+                'bibtex',
+                'rst',
+                'markdown',
+                'markdown_inline',
+                -- git
+                'git_config',
+                'git_rebase',
+                'gitattributes',
+                'gitcommit',
+                'gitignore',
+            },
+        },
+        config = function(_, opts)
+            require('nvim-treesitter.configs').setup(opts)
+        end,
+        opts_extend = { 'ensure_installed' },
     },
-    config = function(_, opts)
-      require('nvim-treesitter.configs').setup(opts)
-    end,
-    opts_extend = { 'ensure_installed' },
-  },
 }
